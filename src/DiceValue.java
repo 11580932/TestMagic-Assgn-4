32eb5bae5a0e4f49feee05ff04d1f60092126bff
11580932 <hari786raj@gmail.com>
2016-10-07 11:27:03 +1100

    Format has been changed

--- src/DiceValue.java
+++ src/DiceValue.java
@@ -4,10 +4,10 @@ import java.util.Random;
 
 public enum DiceValue {
 	CROWN, ANCHOR, HEART, DIAMOND, CLUB, SPADE;
-	
+
 	private static Random RANDOM = new Random();
-	
-	private static final Map<DiceValue, String> VALUE_REPR_MAP= new HashMap<DiceValue, String>();
+
+	private static final Map<DiceValue, String> VALUE_REPR_MAP = new HashMap<DiceValue, String>();
 	static {
 		VALUE_REPR_MAP.put(DiceValue.CROWN, "Crown");
 		VALUE_REPR_MAP.put(DiceValue.ANCHOR, "Anchor");
@@ -16,14 +16,14 @@ public enum DiceValue {
 		VALUE_REPR_MAP.put(DiceValue.CLUB, "Club");
 		VALUE_REPR_MAP.put(DiceValue.SPADE, "Spade");
 	}
-	
+
 	public String toString(DiceValue value) {
 		return VALUE_REPR_MAP.get(value);
 	}
-	
+
 	public static DiceValue getRandom() {
 		int random = RANDOM.nextInt(DiceValue.SPADE.ordinal());
 		return values()[random];
 	}
-	
+
 }
